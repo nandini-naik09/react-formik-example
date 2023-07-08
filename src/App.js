ad69c6e5d4ec0ce7f32d58dbb51fa71bbe1de12c
nandini-naik09 <nandinipardeshi99@gmail.com>
2023-07-08 21:18:00 +0530

    Form Validation Testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,176 @@
-import logo from './logo.svg';
-import './App.css';
+import { React } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 function App() {
+  const validationSchema = Yup.object().shape({
+    fullname: Yup.string().required("Fullname is required"),
+    username: Yup.string()
+      .required("Username is required")
+      .min(6, "Username must be at least 6 characters")
+      .max(20, "Username must not exceed 20 characters"),
+    email: Yup.string().required("Email is required").email("email is invalid"),
+    password: Yup.string()
+      .required("Password is required")
+      .min(6, "Password must be at least 6 characters")
+      .max(40, "Password must not exceed 40 characters"),
+    confirmPassword: Yup.string()
+      .required("Confirm Password is required.")
+      .oneOf([Yup.ref("password"), null], "Confirm password does not match"),
+    acceptTerms: Yup.bool().oneOf([true], "Accept Terms and condition"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      fullname: "",
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      acceptTerms: "false",
+    },
+    validationSchema,
+    onSubmit: (data) => {
+      console.log(JSON.stringify(data, null, 2));
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form onSubmit={formik.handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="fullname">Full Name</label>
+          <input
+            name="fullname"
+            type="text"
+            className={
+              "form-control" +
+              (formik.errors.fullname && formik.touched.fullname
+                ? " is-invalid"
+                : "")
+            }
+            onChange={formik.handleChange}
+            value={formik.values.fullname}
+          />
+          <div className="invalid-feedback">
+            {formik.errors.fullname && formik.touched.fullname
+              ? formik.errors.fullname
+              : null}
+          </div>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="username">UserName</label>
+          <input
+            name="username"
+            type="text"
+            className={
+              "form-control" +
+              (formik.errors.username && formik.touched.username
+                ? " is-invalid"
+                : "")
+            }
+            onChange={formik.handleChange}
+            value={formik.values.username}
+          />
+          <div className="invalid-feedback">
+            {formik.errors.username && formik.touched.username
+              ? formik.errors.username
+              : null}
+          </div>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            name="email"
+            type="text"
+            className={
+              "form-control" +
+              (formik.errors.email && formik.touched.email ? " is-invalid" : "")
+            }
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          />
+          <div className="invalid-feedback">
+            {formik.errors.email && formik.touched.email
+              ? formik.errors.email
+              : null}
+          </div>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            name="password"
+            type="password"
+            className={
+              "form-control" +
+              (formik.errors.password && formik.touched.password
+                ? " is-invalid"
+                : "")
+            }
+            onChange={formik.handleChange}
+            value={formik.values.password}
+          />
+          <div className="invalid-feedback">
+            {formik.errors.password && formik.touched.password
+              ? formik.errors.password
+              : null}
+          </div>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <input
+            name="confirmPassword"
+            type="password"
+            className={
+              "form-control" +
+              (formik.errors.confirmPassword && formik.touched.confirmPassword
+                ? " is-invalid"
+                : "")
+            }
+            onChange={formik.handleChange}
+            value={formik.values.confirmPassword}
+          />
+          <div className="invalid-feedback">
+            {formik.errors.confirmPassword && formik.touched.confirmPassword
+              ? formik.errors.confirmPassword
+              : null}
+          </div>
+        </div>
+
+        <div className="form-group form-check">
+          <input
+            name="acceptTerms"
+            type="checkbox"
+            className={
+              "form-check-input" +
+              (formik.errors.acceptTerms && formik.touched.acceptTerms
+                ? " is-invalid"
+                : "")
+            }
+            onChange={formik.handleChange}
+            value={formik.values.acceptTerms}
+          />
+          <label htmlFor="acceptTerms" className="form-check-label">
+            I have read and agree to the terms
+          </label>
+
+          <div className="invalid-feedback">
+            {formik.errors.acceptTerms && formik.touched.acceptTerms
+              ? formik.errors.acceptTerms
+              : null}
+          </div>
+        </div>
+
+        <div className="form-group">
+          <button type="submit" className="btn btn-primary">
+            Register
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
